c030a5510df142b28ac7a46e7d7110449e029086
luuduc <luuduc1708@gmail.com>
2024-04-30 15:06:50 +0700

    add public folder

--- 1-task-manager/app.js
+++ 1-task-manager/app.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const express = require('express')
+const app = express()
+
+
+// routes
+app.get('/', (req, res) => {
+  res.json('hello')
+})
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`server is listening on http://localhost:${port}`)
+})
